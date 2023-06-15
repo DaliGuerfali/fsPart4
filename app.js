183ed21af7f2c133583d9f04f359c2a08787eaa9
Dali Guerfali <daliguerfali@gmail.com>
2023-06-15 16:36:34 +0100

    router

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 const config = require('./utils/config');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 
+const app = express();
 
 
 console.log('Connecting to db...');
@@ -19,24 +18,7 @@ mongoose.connect(config.MONGODB_URI).then(() => {
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 
 
